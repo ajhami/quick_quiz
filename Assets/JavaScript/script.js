7200388784dc87245ca1f9a82187707bb262b680
Alex Hamilton <alexhami916@gmail.com>
2020-03-21 09:57:22 -0600

    Added pseudocode

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -2,23 +2,38 @@
 /// script.js ///
 /////////////////
 
-var i = 0;
-var myColors = ["#0000ff", "#8a2be8", "#7fff00"];
-var clickTitle = document.getElementById("clickTitle");
+
+/////////////////////
+// Pseudocode
+//
+//
+// variables
+// score, initials
+// maybe questions
+//      Idea
+//          var questions = {
+//                              question : {
+//                                             answer : wrong,
+//                                             answer : correct,
+//                                             answer : wrong,
+//                                             answer : wrong
+//                                         }
+//                              question : {
+//                                             answer : wrong,
+//                                             answer : correct,
+//                                             answer : wrong,
+//                                             answer : wrong
+//                                         }
+//                          }
+//
+// Functions
+// answeredQuestion - attached to eventlistener and adds
+// score for correct answer and reduces points / time 
+// storeScore - take score and initials and store values
+// 
+// Event Listeners
+//
 
 
-clickTitle.addEventListener("click", changeColor);
-
-function changeColor() {
-    i = (i + 1) % 3;
-    console.log("i = " + i);
-    console.log("color code = " + myColors[i]);
-    document.body.style.backgroundColor = myColors[i];
-}
 
 
-/*
-#0000ff
-#8a2be8
-#7fff00
-*/
